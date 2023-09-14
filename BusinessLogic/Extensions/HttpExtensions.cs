b4f4f7987cde6a248a70cebabd33de7579902b14
DzonnyLegend <nikola2000petric@gmail.com>
2023-09-14 21:30:30 +0200

    Sort, Filter, Search and Page features

---
+++ BusinessLogic/Extensions/HttpExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebShop.BusinessLogic.RequestHelpers;
+using WebShop.Data.Model;
+
+namespace WebShop.BusinessLogic.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, MetaData metaData)
+        {
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(metaData, options));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
